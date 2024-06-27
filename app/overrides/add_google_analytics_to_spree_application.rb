91d6c5159876465409ba21c95d090eec48f36430
rahul-bash <rahul-dev@bluebash.co>
2024-06-27 16:34:05 +0530

    Fixed bugs.

--- app/overrides/add_google_analytics_to_spree_application.rb
+++ app/overrides/add_google_analytics_to_spree_application.rb
@@ -3,6 +3,7 @@ if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0.alpha')
     virtual_path: 'spree/shared/_head',
     name: 'add_google_analytics_initializer_to_spree_application',
     insert_after: 'title',
+    original: '2b529599b6a4e9fcc21a295121c7b520e856e1fb',
     text: <<-HTML
       <%= render partial: 'spree/shared/trackers/google_analytics/initializer', formats: :js %>
     HTML
--- app/overrides/add_segment_page_viewed_to_layout.rb
+++ app/overrides/add_segment_page_viewed_to_layout.rb
@@ -3,7 +3,8 @@ unless spree_version >= Gem::Version.create('3.3.0') && spree_version < Gem::Ver
   Deface::Override.new(
     virtual_path: 'spree/shared/_head',
     name: 'add_segment_page_tracker_to_body',
-    insert_before: 'meta',
+    insert_before: 'meta[name="viewport"]',
+    original: 'fe1629c287f8302be09a6e085497f082bdf0de2a',
     text: <<-HTML
       <%= render partial: 'spree/shared/trackers/segment/page_viewed', formats: :js %>
     HTML
