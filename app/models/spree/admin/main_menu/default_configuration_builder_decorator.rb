14676e6105eb7b6b77df8cf31df2eabf4ae240e0
Shrey Gupta <shreygupta@Spiderman.local>
2024-12-09 15:45:36 +0530

    Sidebar fixes

--- app/models/spree/admin/main_menu/default_configuration_builder_decorator.rb
+++ app/models/spree/admin/main_menu/default_configuration_builder_decorator.rb
@@ -2,15 +2,15 @@ module Spree
   module Admin
     module MainMenu
       module DefaultConfigurationBuilderDecorator
-        def add_settings_section(root)
+        def add_integrations_section(root)
           super
 
-          section = root.items.find { |s| s.key == 'settings' }
+          section = root.items.find { |s| s.key == 'integrations' }
           return unless section
 
           section.items << ItemBuilder.new(Spree.t(:analytics), admin_trackers_path).
-            with_manage_ability_check(Spree::Config).
-            with_match_path(admin_trackers_path).
+            with_manage_ability_check(Spree::Tracker).
+            with_match_path('/analytics').
             build
         end
       end
